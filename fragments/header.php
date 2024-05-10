46e96f046089d1cc27ee02b09a73a17ff5382b29
Colin Sweetland <csweetla@student.fitchburgstate.edu>
2024-05-10 12:28:30 -0400

    admin panel link in nav when logged in as admin

--- fragments/header.php
+++ fragments/header.php
@@ -14,35 +14,42 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="browse_games.php" class="nav-link px-2 text-white">Games</a></li>
                 <li><a href="#" class="nav-link px-2 text-white">Recent</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+                <?php if ($_SESSION['user_type'] == 'admin') : ?>
+                    <li>   
+                        <a href="/admin_panel.php" class="nav-link px-2 text-white" >
+                            Admin Panel
+                        </a>
+                    </li>
+                <?php endif; ?>
             </ul>
 
-            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <form class="col-12 col-lg-auto mb-3 mb-lg me-2" role="search">
                 <div class="input-group px-4">
                     <input type="search" class="form-control form-control-dark bg-light text-bg-light" placeholder="Search.." aria-label="Search">
-                      <button class="btn btn-primary" type="button" id="button-addon1">Search</button>
+                    <button class="btn btn-primary" type="button" id="button-addon1">Search</button>
                 </div>
             </form>
 
             <div class="text-end">
 
 
-                
+
                 <?php if (empty($_SESSION['user_id'])) : ?>
-                
-                <a href="register.php" class="btn btn-primary"> 
-                    Register
-                </a>
-                
-                <a href="login.php" class="btn btn-outline-light me-2"> 
-                    Login
-                </a>
-                
+
+                    <a href="register.php" class="btn btn-primary"> 
+                        Register
+                    </a>
+
+                    <a href="login.php" class="btn btn-outline-light me-2"> 
+                        Login
+                    </a>
+
                 <?php else : ?>
-               
-                <a href="<?php echo "actions/logout_action.php?" . http_build_query(["back" => $_SERVER['REQUEST_URI']]) ?>" class="btn btn-secondary me-2" >
-                    Logout (Logged in as <b><?php echo htmlspecialchars($_SESSION['username']) ?> </b> )
-                </a>
+
+                    <a href="<?php echo "actions/logout_action.php?" . http_build_query(["back" => $_SERVER['REQUEST_URI']]) ?>" class="btn btn-secondary me-2" >
+                        Logout (Logged in as <b><?php echo htmlspecialchars($_SESSION['username']) ?> </b> )
+                    </a>
+
                 <?php endif; ?>
             </div>
         </div>
